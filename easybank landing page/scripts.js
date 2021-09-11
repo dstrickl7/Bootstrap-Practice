deb19e1867ce7ac229e9f00df23a9c7eea256821
Danielle S <strickl6@illinois.edu>
2021-09-11 13:19:03 -0500

    completed desktop layout for article section

--- easybank landing page/scripts.js
+++ easybank landing page/scripts.js
@@ -1,7 +1,9 @@
 const close = document.querySelector(".navbar-toggler-icon");
 const overlay = document.querySelector(".overlay");
+const navlist = document.querySelector(".nav-list-container");
 
 close.addEventListener("click", () => {
   close.classList.toggle("active");
   overlay.classList.toggle("active");
+  navlist.classList.toggle("active");
 });
