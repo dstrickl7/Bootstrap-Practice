ada77bb0a0c017caba508608f3434da19bb671b4
Danielle S <strickl6@illinois.edu>
2021-10-21 16:45:23 -0500

    fixed issue where mobile nav appeared on desktop

--- easybank landing page/scripts.js
+++ easybank landing page/scripts.js
@@ -2,23 +2,25 @@ const close = document.querySelector(".navbar-toggler-icon");
 const overlay = document.querySelector(".overlay");
 const navlist = document.querySelector(".nav-list-container");
 
-close.addEventListener("click", () => {
-  close.classList.toggle("active");
-  overlay.classList.toggle("active");
-  navlist.classList.toggle("active");
-});
+if (document.documentElement.clientWidth < 992) {
+  close.addEventListener("click", () => {
+    close.classList.toggle("active");
+    overlay.classList.toggle("active");
+    navlist.classList.toggle("active");
+  });
 
-// Attempt to close navlist when a link is clicked
-let navlink = document.querySelectorAll(".nav-link");
+  // Attempt to close navlist when a link is clicked
+  let navlink = document.querySelectorAll(".nav-link");
 
-navlink.forEach((link) =>
-  link.addEventListener("click", () => {
-    close.click();
-  })
-);
+  navlink.forEach((link) =>
+    link.addEventListener("click", () => {
+      close.click();
+    })
+  );
 
-overlay.addEventListener("click", () => {
-  if (overlay.classList.contains("active")) {
-    close.click();
-  }
-});
+  overlay.addEventListener("click", () => {
+    if (overlay.classList.contains("active")) {
+      close.click();
+    }
+  });
+}
